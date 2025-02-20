7f024770c378b5d4d7a4afbe950dd62b3e58d7cc
Afiq <afiqroslin97@gmail.com>
2025-02-20 20:45:00 +0800

    add post features

--- index.js
+++ index.js
@@ -16,15 +16,30 @@ app.get("/", (req, res) => {
   res.render("index.ejs", { posts: posts});
   });
 
-app.post("/submit", (req, res) => {
-
-  res.render("index.ejs");
-});
-
+// Post page
 app.get("/post", (req, res) => {
 
   res.render("post.ejs");
+  });
+
+
+// Submit posts
+app.post("/posts", (req, res) => {
+
+  const { btitle, bcontent } = req.body;
+
+  const newPost = {
+    title: btitle,
+    content: bcontent,
+    date: new Date().toLocaleString(),
+  };
+
+  posts.push(newPost);
+
+  res.redirect("/");
 });
+
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
